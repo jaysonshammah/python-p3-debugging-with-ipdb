b0f317d328e4a7d95b47b0325edb07f4499dbb60
jayson <jaysonshammah3@gmail.com>
2024-05-22 12:21:45 +0300

    add change

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -4,8 +4,8 @@ import ipdb
 
 def plus_two(num):
     num +=2
-    ipdb.set_trace()
+    # ipdb.set_trace()
     return num
 
 result = plus_two(3)
-print("Result: " + result)
+# print("Result: " + result)
